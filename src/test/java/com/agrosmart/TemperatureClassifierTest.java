546e96d243dac7028989394eed53fb5505a581d4
DevJ25 <alavrezsanti@gmail.com>
2026-09-05 21:16:03 -0500

    fix: corregir prueba unitaria y restaurar pipeline CI/CD en GitHub Actions

--- src/test/java/com/agrosmart/TemperatureClassifierTest.java
+++ src/test/java/com/agrosmart/TemperatureClassifierTest.java
@@ -13,9 +13,7 @@ class TemperatureClassifierTest {
 
     @Test
     void debeClasificarComoNormalHasta8Grados() {
-        // [MODIFICACIÓN INTENCIONAL DE PRÁCTICA]: Cambio de SensorStatus.NORMAL a SensorStatus.ALERT
-        // Esto provocará un error en 'mvn test' y hará que el pipeline de GitHub Actions falle.
-        assertEquals(SensorStatus.ALERT, TemperatureClassifier.classify(6.0));
+        assertEquals(SensorStatus.NORMAL, TemperatureClassifier.classify(6.0));
         assertEquals(SensorStatus.NORMAL, TemperatureClassifier.classify(8.0));
     }
 
